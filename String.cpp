e9783f174e4dd946d3b0a29004b7c7009b58561d
Nand0027 <nand.kishor4844@gmail.com>
2025-08-02 23:49:21 +0530

    update code

--- String.cpp
+++ String.cpp
@@ -68,49 +68,51 @@
 
 
 // Check palindrome
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-bool valid(char ch) {
-    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
-}
+// bool valid(char ch) {
+//     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+// }
 
-char toLowerCase(char ch) {
-    if (ch >= 'a' && ch <= 'z') {
-        return ch;
-    } else {
-        return ch - 'A' + 'a';
-    }
-}
+// char toLowerCase(char ch) {
+//     if (ch >= 'a' && ch <= 'z') {
+//         return ch;
+//     } else {
+//         return ch - 'A' + 'a';
+//     }
+// }
 
-bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length() - 1;  // Fix: Correct the last index
+// bool checkPalindrome(string a) {
+//     int s = 0;
+//     int e = a.length() - 1;  // Fix: Correct the last index
 
-    while (s <= e) {  // Fix: Should be <= to include the middle character
-        if (a[s] != a[e]) {
-            return false;
-        }
-        s++;
-        e--;
-    }
-    return true;
-}
+//     while (s <= e) {  // Fix: Should be <= to include the middle character
+//         if (a[s] != a[e]) {
+//             return false;
+//         }
+//         s++;
+//         e--;
+//     }
+//     return true;
+// }
 
-bool isPalindrome(string s) {
-    // Remove unnecessary characters
-    string temp = "";
-    for (int i = 0; i < s.length(); i++) {
-        if (valid(s[i])) {
-            temp.push_back(toLowerCase(s[i]));  // Fix: Convert to lowercase while adding
-        }
-    }
+// bool isPalindrome(string s) {
+//     // Remove unnecessary characters
+//     string temp = "";
+//     for (int i = 0; i < s.length(); i++) {
+//         if (valid(s[i])) {
+//             temp.push_back(toLowerCase(s[i]));  // Fix: Convert to lowercase while adding
+//         }
+//     }
 
-    // Check palindrome
-    return checkPalindrome(temp);
-}
+//     // Check palindrome
+//     return checkPalindrome(temp);
+// }
 
-int main() {
-    string d = "A man, a plan, a canal: Panama";
-    cout << (isPalindrome(d) ? "true" : "false") << endl;  // Fix: Better output
-}
+// int main() {
+//     string d = "A man, a plan, a canal: Panama";
+//     cout << (isPalindrome(d) ? "true" : "false") << endl;  // Fix: Better output
+// }
+
+
